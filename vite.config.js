9cb84c284936e11c7867878cdabee12d6501cb67
Begoña López <begolopez65@gmail.com>
2025-12-20 11:48:57 +0100

    Fix: Configuración de Vite corregida para multipágina

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,15 @@
+import { resolveConfig } from "vite";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 export default defineConfig({
   base: "/bla-modern-art-gallery",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        segunda: resolve(__dirname, "location.html"),
+      },
+    },
+  },
 });
